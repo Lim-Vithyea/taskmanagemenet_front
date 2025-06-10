95ee9b84b95c6e764399d3ab5ffa4dcf2c3c60dc
Lim Vithyea <cheakh212@gmail.com>
2025-06-10 08:44:35 +0700

    small changes

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import Profilepic from "../assets/pfpic.jpg"
 import { Facebook, Github } from 'lucide-react';
 
-const ProfileCard = ({name,pic}) => {
+const ProfileCard = ({name,pic,facbookLink,githubLink}) => {
   return (
-        <div class="w-[300px] max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm ">
-            <div class="flex justify-end px-4 pt-4">
+        <div className="w-[300px] max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm 
+        transform transition-transform duration-300 hover:scale-105 hover:shadow-xl ">
+            <div className="flex justify-end px-4 pt-4">
             </div>
-            <div class="flex flex-col items-center pb-10">
-                <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={pic} alt="Bonnie image"/>
-                <h5 class="mb-1 text-xl font-medium text-gray-900">{name}</h5>
-                <span class="text-sm text-gray-500 dark:text-gray-400">IT Student</span>
-                <div class="flex mt-4 md:mt-6">
-                    <a href="#" class="inline-flex items-center px-5 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <div className="flex flex-col items-center pb-10">
+                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={pic} alt="Bonnie image"/>
+                <h5 className="mb-1 text-xl font-medium text-gray-900">{name}</h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">IT Student</span>
+                <p className='text-center text-sm py-2 text-gray-500'>Even though you don't give up doesn't mean you will make it</p>
+                <div className="flex mt-4 md:mt-2">
+                    <a href={facbookLink} className="inline-flex items-center px-5 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <Facebook className='w-5 h-5 mx-1'/>Faecbook</a>
-                    <a href="#" class="py-2 px-4 ms-2 text-sm font-medium text-white focus:outline-none flex 
+                    <a href={githubLink} className="py-2 px-4 ms-2 text-sm font-medium text-white focus:outline-none flex 
                     rounded-lg border border-gray-800 bg-gray-800 hover:bg-gray-600 
                     focus:z-10 focus:ring-4 focus:ring-gray-100 ">
                         <Github className='w-5 h-5 '/>
--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -5,19 +5,25 @@ import Samaunpic from '../assets/samaun.jpg'
 import Pfpic from "../assets/pfpic.jpg"
 
 const TeamData = [
-    {id:1,name:"Lim Vithyea",pic:Vithyeapic},
-    {id:2,name:"Doem Samaun",pic:Samaunpic},
-    {id:3,name:"Vannon",pic:Pfpic},
-    {id:4,name:"Thuon Sithun",pic:Pfpic},
+    {id:1,name:"Lim Vithyea",pic:Vithyeapic,facebooklink: "https://www.facebook.com/lim.vithyea.58",github:"https://github.com/Lim-Vithyea"},
+    {id:2,name:"Doem Samaun",pic:Samaunpic,facebooklink:"https://www.facebook.com/z90xfiu4u1",github:"https://github.com/Lim-Vithyea"},
+    {id:3,name:"Vannon",pic:Pfpic,facebooklink: "https://www.facebook.com/lim.vithyea.58",github:"https://github.com/Lim-Vithyea"},
+    {id:4,name:"Thuon Sithun",pic:Pfpic,facebooklink: "https://www.facebook.com/lim.vithyea.58",github:"https://github.com/Lim-Vithyea"},
 ]
 
 const OurTeam = () => {
   return (
     <div className="flex flex-col items-center py-5 px-4">
-      <h1 className="text-3xl font-semibold text-center mb-8">Meet our Team</h1>
+      <h1 className="text-2xl font-semibold text-center mb-8">Meet our awsome Team</h1>
       <div className="grid grid-cols-2 gap-6">
        {TeamData.map((data) => (
-            <ProfileCard key={data.id} name={data.name} pic={data.pic} />
+            <ProfileCard 
+            key={data.id} 
+            name={data.name} 
+            pic={data.pic} 
+            facbookLink={data.facebooklink}
+            githubLink={data.github}
+            />
         ))}  
       </div>
     </div>
