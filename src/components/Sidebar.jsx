55ee1211d616f98f437d2fc46feaea35b9340bb6
Lim Vithyea <cheakh212@gmail.com>
2025-07-01 15:33:36 +0700

    small change

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,13 +20,9 @@ const Sidebar = () => {
         <li>
           <NavLink
             to="/dashboard/taskmanage"
-            className={({ isActive }) =>
-              `flex items-center p-2 rounded transition-colors ${
-                isActive
+            className={({ isActive }) => `flex items-center p-2 rounded transition-colors ${isActive 
                   ? 'bg-blue-100 text-blue-800 font-semibold'
-                  : 'text-gray-800 hover:bg-blue-50 hover:text-blue-800'}`
-            }
-          >
+                  : 'text-gray-800 hover:bg-blue-50 hover:text-blue-800'}`}>
             <svg className="w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 22 21" xmlns="http://www.w3.org/2000/svg">
               <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
               <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
@@ -38,13 +34,9 @@ const Sidebar = () => {
         <li className={user?.role === "2" ? "hidden" : ""}>
           <NavLink
             to="/dashboard/usersection"
-            className={({ isActive }) =>
-              `flex items-center p-2 rounded transition-colors ${
-                isActive
+            className={({ isActive }) =>`flex items-center p-2 rounded transition-colors ${isActive
                   ? 'bg-blue-100 text-blue-800 font-semibold'
-                  : 'text-gray-800 hover:bg-blue-50 hover:text-blue-800'}`
-            }
-          >
+                  : 'text-gray-800 hover:bg-blue-50 hover:text-blue-800'}`}>
             <svg className="w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 18" xmlns="http://www.w3.org/2000/svg">
               <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
             </svg>
@@ -56,12 +48,9 @@ const Sidebar = () => {
           <NavLink
             to="/dashboard/report"
             className={({ isActive }) =>
-              `flex items-center p-2 rounded transition-colors ${
-                isActive
+              `flex items-center p-2 rounded transition-colors ${isActive
                   ? 'bg-blue-100 text-blue-800 font-semibold'
-                  : 'text-gray-800 hover:bg-blue-50 hover:text-blue-800'}`
-            }
-          >
+                  : 'text-gray-800 hover:bg-blue-50 hover:text-blue-800'}`}>
             <ClipboardMinus className="w-5 h-5 mr-3" />
             Report
           </NavLink>
@@ -71,12 +60,9 @@ const Sidebar = () => {
           <NavLink
             to="/dashboard/aboutus"
             className={({ isActive }) =>
-              `flex items-center p-2 rounded transition-colors ${
-                isActive
+              `flex items-center p-2 rounded transition-colors ${isActive
                   ? 'bg-blue-100 text-blue-800 font-semibold'
-                  : 'text-gray-800 hover:bg-blue-50 hover:text-blue-800'}`
-            }
-          >
+                  : 'text-gray-800 hover:bg-blue-50 hover:text-blue-800'}`}>
             <UsersRound className="w-5 h-5 mr-3" />
             Our Team
           </NavLink>
